f30000c33fca628a47c299362b380e980c116e64
sluo10 <sam.luo.ee@gmail.com>
2021-06-09 22:57:49 -0500

    Refactored implementation to not use callbacks, added typing support for chainlink library

--- index.ts
+++ index.ts
@@ -1,78 +1,66 @@
-const { Requester, Validator } = require('@chainlink/external-adapter') // no @types packages for this?
-import { Callback, Context } from 'aws-lambda'
-// import dotenv from 'dotenv'
-// dotenv.config()
+import { Callback, Context } from 'aws-lambda';
+import { IRequesterResponse, getValidatorWrapper, IRequestInput, ICustomError, RequesterRequestWrapper, RequesterErroredWrapper } from './types/chainlink-adapter'
 
-interface iCustomError {
-  Response: string
+interface ICreateRequestResponse extends IRequesterResponse {
+  jobRunID: string;
 }
 
-// Define custom error scenarios for the API.
-// Return true for the adapter to retry.
-const customError = (data: iCustomError) => {
-  if (data.Response === 'Error') return true
-  return false
-}
+export const createRequest = async (input: IRequestInput): Promise<ICreateRequestResponse> => {
+  var jobRunID = ''
+  try {
+    const customParams = {
+      tokenIdInt: 'tokenIdInt',
+      tickSet: 'tickSet',
+    };
+    const validator = getValidatorWrapper(input, customParams);
+    jobRunID = validator.validated.id;
 
-// Define custom parameters to be used by the adapter.
-// Extra parameters can be stated in the extra object,
-// with a Boolean value indicating whether or not they
-// should be required.
-const customParams = {
-  tokenIdInt: 'tokenIdInt',
-  tickSet: 'tickSet'
-}
+    const config = {
+      url: 'https://xzff24vr3m.execute-api.us-east-2.amazonaws.com/default/spectral-proxy/',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': 'XYpX4gaNaCafgAzdBpQyaLrF34N0Qp71N6qOwvSh',
+      },
+      data: `{\"tokenInt\":\"${validator.validated.data.tokenIdInt}\"}`,
+      method: 'POST',
+      timeout: 30000,
+    };
 
-// TODO: add types
-export const createRequest = (input: any, callback: any) => {
-  // The Validator helps you validate the Chainlink request data
-  console.log("inputs: ", input)
-  const validator = new Validator(input, customParams)
-  console.log("validator: ", validator.validated)
-  console.log("inputed data: ", validator.validated.data)
-  const jobRunID = validator.validated.id
-  const tokenIdInt = validator.validated.data.tokenIdInt
-  const tickSet = validator.validated.data.tickSet
-  const url = `https://xzff24vr3m.execute-api.us-east-2.amazonaws.com/default/spectral-proxy/`
-
-  const config = {
-    url,
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': 'XYpX4gaNaCafgAzdBpQyaLrF34N0Qp71N6qOwvSh',
-    },
-    data: `{\"tokenInt\":\"${tokenIdInt}\"}`,
-    method: 'POST',
-    timeout: 30000
+    const customError = (data: ICustomError) => {
+      if (data.Response === 'Error') return true;
+      return false;
+    };
+    const response = await RequesterRequestWrapper(config, customError);
+    if (response.data?.length > 0) {
+      return { jobRunID, ...response };
+    } else {
+      throw RequesterErroredWrapper(jobRunID, new Error("Response did not contain score data!"), 400);
+    }
+  } catch (error) {
+    throw RequesterErroredWrapper(jobRunID, error, 500);
   }
+};
 
-  console.log("config for post: ", config)
-
-  Requester.request(config, customError)
-    .then((response: any) => {
-      //manage ticksets here
-      callback(
-        response.status,
-        Requester.success(jobRunID, {
-          jobRunID: input.id,
-          data: { result: response.data },
-          statusCode: 200,
-        }),
-      )
-    })
-    .catch((error: any) => {
-      callback(500, Requester.errored(jobRunID, error))
-    })
-}
-
-// This is a wrapper to allow the function to work with
-// newer AWS Lambda implementations
+// This is a wrapper to allow the function to work with newer AWS Lambda implementations
 exports.handlerv2 = (event: any, context: Context, callback: Callback) => {
-  createRequest(JSON.parse(event.body), (statusCode: number, data: any) => {
-    callback(null, {
-      statusCode: statusCode,
-      body: JSON.stringify(data),
-      isBase64Encoded: false,
+  try {
+    var input = JSON.parse(event.body);
+  } catch (err) {
+    callback(new Error('Error parsing body!'), {
+      statusCode: 500,
+    });
+  }
+  createRequest(input)
+    .then((result) => {
+      callback(null, {
+        statusCode: result.status,
+        body: JSON.stringify(result.data),
+        isBase64Encoded: false,
+      });
     })
-  })
-}
+    .catch((error) => {
+      callback(error, {
+        statusCode: error?.statusCode ?? 500,
+      });
+    });
+};
